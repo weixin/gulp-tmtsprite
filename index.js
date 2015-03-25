3e95c66cf0def447c4220142ee805df3ae353954
willerce <willerce@gmail.com>
2015-03-25 19:31:47 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 'use strict';
 
 var sprite = require('./lib/sprite');
-var through2 = require('through2');
-var vfs = require('vinyl-fs');
-var fs = require('graceful-fs');
-var mkdirp = require('mkdirp');
-var path = require('path');
-var _ = require('lodash');
 
 module.exports = function (opts) {
   return sprite(opts);
