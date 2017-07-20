9afda5feb6e1c9a265503f19733fcf61237926db
conechan <conechan@gmail.com>
2017-07-20 17:17:00 +0800

    use jimp instead of lwip.

--- test/tmtsprite.js
+++ test/tmtsprite.js
@@ -5,11 +5,23 @@ var sprite = require('../lib/sprite');
 var path = require('path');
 var vfs = require('vinyl-fs');
 var through2 = require('through2');
-var lwip = require('lwip');
 var noop = function () {
 };
 
 require('mocha');
 
 describe('css-sprite (lib/sprite.js)', function () {
+
+  it('should have a result', function (cb) {
+    this.timeout(10000);
+    var stream = sprite();
+
+
+    vfs.src([path.join(__dirname, './css/style-willerce.css')])
+      .pipe(stream)
+      .pipe(vfs.dest(path.join(__dirname, './result')))
+      .on('end', cb);
+
+  })
+
 });
